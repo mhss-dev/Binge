be6fcc2a20ffa7604e2651e793e5695b1388e2f6
mhss-dev <you@example.com>
2024-08-06 00:39:53 +0200

    cards

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardsComponent } from './cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
