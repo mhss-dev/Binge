c6276342d58e2e6132ffc22f856ac5741551e2e3
mhss-dev <hedi.mhss@gmail.com>
2024-09-05 19:01:18 +0200

    ios app arc???

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.binge.app',
+  appName: 'Binge',
+  webDir: 'dist/binge/browser'
+};
+
+export default config;
