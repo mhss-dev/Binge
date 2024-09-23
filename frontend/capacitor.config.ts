09c21668a6b858e564323e16147592f9979b905b
hedi <hedi.mhss@gmail.com>
2024-09-23 13:28:12 +0200

    filmography actor fix it was 12 items max

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.binge.app',
   appName: 'Binge',
-  webDir: 'dist/binge/browser'
-};
+  webDir: 'dist/binge/browser',
+}
 
 export default config;
