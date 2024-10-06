5340eae8bef6b6ba1a0082a15250ecaec7e57846
mhss-dev <hedi.mhss@gmail.com>
2024-10-06 04:20:24 +0200

    members stats count

--- frontend/src/app/members/members.component.ts
+++ frontend/src/app/members/members.component.ts
@@ -1,7 +1,12 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { MembersService } from '../members.service';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FavoritesService } from 'app/favorites.service';
+import { WatchedService } from 'app/watched.service';
+import { WatchlistService } from 'app/watchlist.service';
+import { DetailsService } from 'app/details.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-members',
@@ -13,10 +18,32 @@ import { RouterLink } from '@angular/router';
 
 export class MembersComponent {
   members : any[] = [];
+
+  favoritesCount: number = 0;
+  watchedCount: number = 0;
+  watchlistCount: number = 0;
   
-  constructor(private MembreService: MembersService, private cdr: ChangeDetectorRef) {}
+  favorites: any[] = [];
+  watched: any[] = [];
+  watchlist: any[] = [];
+  
+  constructor(
+    private MembreService: MembersService, 
+    private favoritesService: FavoritesService,
+    private detailsService: DetailsService,
+    private watchedService: WatchedService,
+    private watchlistService: WatchlistService,
+    private cdr: ChangeDetectorRef,
+    private route: ActivatedRoute,
+
+
+  ) {}
 
   ngOnInit() : void {
+
+      this.fetchFavorites();
+      this.fetchWatched();
+      this.fetchWatchlist();
 
     this.MembreService.getMembers().subscribe({
       next: (response) => {
@@ -27,6 +54,59 @@ export class MembersComponent {
         console.error('Erreur lors de la récupération des membres:', err)
       }
   })
+}
+
+fetchFavorites(): void {
+  const routeNickname = this.route.snapshot.paramMap.get('nickname');
+  
+  this.favoritesService.getFavorites(routeNickname || undefined).subscribe({
+    next: (favorites: any[]) => {
+      this.favoritesCount = favorites.length;
+      const movieIds = favorites.map(fav => fav.movie_id);
+      const requests = movieIds.map(id => this.detailsService.getMovieByID(id));
+      
+      forkJoin(requests).subscribe({
+        next: (movies) => {
+          this.favorites = movies;
+          this.cdr.detectChanges();
+        },
+        error: (err) => {
+          console.error('Fetching favoris erreur :', err);
+        }
+      });
+    },
+    error: (err) => {
+      console.error('Fetching favoris erreur :', err);
+    }
+  });
+}
+
+fetchWatched(): void {
+  const routeNickname = this.route.snapshot.paramMap.get('nickname');
+  
+  this.watchedService.getWatched(routeNickname || undefined).subscribe({
+    next: (watched: any[]) => {
+      this.watchedCount = watched.length; 
+      this.cdr.detectChanges();
+    },
+    error: (err) => {
+      console.error('Fetching watched erreur :', err);
+    }
+  });
+}
+
+fetchWatchlist(): void {
+  const routeNickname = this.route.snapshot.paramMap.get('nickname');
+  
+  this.watchlistService.getWatchlist(routeNickname || undefined).subscribe({
+    next: (watchlist: any[]) => {
+      this.watchlistCount = watchlist.length; 
+      this.cdr.detectChanges();
+    },
+    error: (err) => {
+      console.error('Fetching watchlist erreur :', err);
+    }
+  });
 }
     
 }
