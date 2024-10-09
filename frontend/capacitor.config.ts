cc8f939a894464aa9a7f4af13750c86855ab5565
hedi <hedi.mhss@gmail.com>
2024-10-09 04:39:09 +0200

    fixing dropdown?

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'com.binge.app',
   appName: 'Binge',
   webDir: 'dist/binge/browser',
+server: {
+  url: 'http://192.168.0.118:4200', // Use your local IP address here
+  cleartext: true
 }
-
+}
 export default config;
