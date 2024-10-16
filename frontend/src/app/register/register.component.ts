1ac933ab1b1a697fa68f58c0f22ae4c5da0224f7
mhss-dev <hedi.mhss@gmail.com>
2024-10-16 04:04:08 +0200

    cleaning login

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -23,26 +23,24 @@ export class RegisterComponent {
 
   onRegister(): void {
     this.authService.register(this.username, this.password, this.nickname).subscribe({
-      next: (response) => {
-        if (response.status === 409) {
+      next: () => {
+        this.registerMessage = "Inscription réussie, bienvenue " + this.nickname + ' !';
+        this.alertType = 'alert-success';
+        setTimeout(() => this.router.navigate(['/login']), 3000);
+      },
+      error: (error) => {
+        if (error.status === 409) {
           this.registerMessage = "Ce nom de compte ou pseudo existe déjà";
           this.alertType = 'alert-danger';
-        } else if (response.status === 200) {
-          this.registerMessage = "Inscription avec succès. Bienvenue " + this.nickname;
-          this.alertType = 'alert-success';
-          setTimeout(() => this.router.navigate(['/profil']), 3000)
-        } else if (response.status === 400) {
+        } else if (error.status === 400) {
           this.registerMessage = 'Tous les champs doivent être indiqués.';
           this.alertType = 'alert-danger';
         } else {
           this.registerMessage = 'Une erreur a eu lieu, réessayez.';
           this.alertType = 'alert-danger';
         }
-      },
-      error: (error) => {
-        this.registerMessage = 'Une erreur a eu lieu, réessayez.';
-        this.alertType = 'alert-danger'; 
       }
     });
   }
+  
 }
