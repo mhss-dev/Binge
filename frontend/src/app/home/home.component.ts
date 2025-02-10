0a70242f2d6d0313a31559d3ad003d1adf6f7d84
mhss-dev <hedi.mhss@gmail.com>
2025-02-10 06:41:44 +0100

    home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,22 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { CardsComponent } from '../cards/cards.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../navbar/navbar.component'; 
+import { NavbarComponent } from '../navbar/navbar.component';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HeaderComponent, CardsComponent, RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
-
-  constructor(private NavbarComponent: NavbarComponent) {}
+  constructor(
+    private NavbarComponent: NavbarComponent,
+    private titleService: Title
+  ) {}
 
   ngOnInit(): void {
-    // this.NavbarComponent.navbarClass = 'navbar-home'; 
+    this.titleService.setTitle('Binge • social & découverte de films');
   }
-
 }
